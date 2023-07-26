0ad112449c0c1c8b8f77dd09c6895cf26725cad0
Vinayak-Asati <vinayakasaticoding@gmail.com>
2023-07-26 15:48:26 +0530

    SpeedSense

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div class="temp">
+    <h1>
+      Expense Tracker
+    </h1>
+    <h3>
+      Track your Day to Day Expense
+    </h3>
+    </div>
+  )
+}
